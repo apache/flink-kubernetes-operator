e96f5b2d74aa70ae27a9d9d08ef0a7a1a06bd137
Marton Balassi <marton@apple.com>
2022-03-15 21:28:43 +0100

    [FLINK-26612] Documentation build via GitHub actions
    Based on zentol's work in https://github.com/apache/flink/pull/19103/commits/54678bbe907d6f0dc70da226b0209e38fa06c59e.

--- .github/workflows/docs.sh
+++ .github/workflows/docs.sh
@@ -16,22 +16,36 @@
 #  See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
+set -e
 
+export JAVA_HOME=$JAVA_HOME_11_X64
+
+# setup hugo
 HUGO_REPO=https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_extended_0.80.0_Linux-64bit.tar.gz
 HUGO_ARTIFACT=hugo_extended_0.80.0_Linux-64bit.tar.gz
-
 if ! curl --fail -OL $HUGO_REPO ; then
 	echo "Failed to download Hugo binary"
 	exit 1
 fi
-
-tar -zxvf $HUGO_ARTIFACT hugo
-
+tar -zxvf $HUGO_ARTIFACT
 git submodule update --init --recursive
 # generate docs into docs/target
 ./hugo -v --source docs --destination target
-
 if [ $? -ne 0 ]; then
 	echo "Error building the docs"
 	exit 1
 fi
+
+# build Flink Operator; required for Javadoc step
+#mvn clean install -B -DskipTests
+
+# build Java docs
+#mkdir -p docs/target/api
+#mvn javadoc:aggregate -B \
+#    -DadditionalJOption="-Xdoclint:none --allow-script-in-comments" \
+#    -Dmaven.javadoc.failOnError=false \
+#    -Dcheckstyle.skip=true \
+#    -Dspotless.check.skip=true \
+#    -Denforcer.skip=true \
+#    -Dheader="<a href=\"http://flink.apache.org/\" target=\"_top\"><h1>Back to Flink Website</h1></a> <script>var _paq=window._paq=window._paq||[];_paq.push([\"disableCookies\"]),_paq.push([\"setDomains\",[\"*.flink.apache.org\",\"*.nightlies.apache.org/flink\"]]),_paq.push([\"trackPageView\"]),_paq.push([\"enableLinkTracking\"]),function(){var u=\"//matomo.privacy.apache.org/\";_paq.push([\"setTrackerUrl\",u+\"matomo.php\"]),_paq.push([\"setSiteId\",\"1\"]);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s)}();</script>"
+#mv target/site/apidocs docs/target/api/java
