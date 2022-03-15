81f6d740668e49924db149204b7261135d8e2a11
Marton Balassi <marton@apple.com>
2022-03-15 20:35:22 +0100

    [FLINK-26612] Documentation build via GitHub actions
    Based on zentol's work in https://github.com/apache/flink/pull/19103/commits/54678bbe907d6f0dc70da226b0209e38fa06c59e.

--- .github/workflows/docs.sh
+++ .github/workflows/docs.sh
@@ -16,22 +16,41 @@
 #  See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
+set -e
 
+export JAVA_HOME=$JAVA_HOME_11_X64
+
+mvn --version
+java -version
+javadoc -J-version
+
+# setup hugo
 HUGO_REPO=https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_extended_0.80.0_Linux-64bit.tar.gz
 HUGO_ARTIFACT=hugo_extended_0.80.0_Linux-64bit.tar.gz
-
 if ! curl --fail -OL $HUGO_REPO ; then
 	echo "Failed to download Hugo binary"
 	exit 1
 fi
-
-tar -zxvf $HUGO_ARTIFACT hugo
-
+tar -zxvf $HUGO_ARTIFACT
 git submodule update --init --recursive
 # generate docs into docs/target
 ./hugo -v --source docs --destination target
-
 if [ $? -ne 0 ]; then
 	echo "Error building the docs"
 	exit 1
 fi
+
+# build Flink Operatpr; required for Javadoc step
+/usr/lib/jvm/adoptopenjdk-11-hotspot-amd64 mvn clean install -B -DskipTests
+
+# build java/scala docs
+mkdir -p docs/target/api
+mvn javadoc:aggregate -B \
+    -Paggregate-scaladoc \
+    -DadditionalJOption="-Xdoclint:none --allow-script-in-comments" \
+    -Dmaven.javadoc.failOnError=false \
+    -Dcheckstyle.skip=true \
+    -Dspotless.check.skip=true \
+    -Denforcer.skip=true \
+    -Dheader="<a href=\"http://flink.apache.org/\" target=\"_top\"><h1>Back to Flink Website</h1></a> <script>var _paq=window._paq=window._paq||[];_paq.push([\"disableCookies\"]),_paq.push([\"setDomains\",[\"*.flink.apache.org\",\"*.nightlies.apache.org/flink\"]]),_paq.push([\"trackPageView\"]),_paq.push([\"enableLinkTracking\"]),function(){var u=\"//matomo.privacy.apache.org/\";_paq.push([\"setTrackerUrl\",u+\"matomo.php\"]),_paq.push([\"setSiteId\",\"1\"]);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s)}();</script>"
+mv target/site/apidocs docs/target/api/java
