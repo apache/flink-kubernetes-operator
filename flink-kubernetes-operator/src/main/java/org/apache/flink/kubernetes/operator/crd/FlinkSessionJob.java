cdaa9b52ed90fe3f903f29e6d09db89a7b9eaed0
Gyula Fora <g_fora@apple.com>
2022-04-06 11:13:06 +0200

    [FLINK-27005] Bump CRD version to v1beta1

--- flink-kubernetes-operator/src/main/java/org/apache/flink/kubernetes/operator/crd/FlinkSessionJob.java
+++ flink-kubernetes-operator/src/main/java/org/apache/flink/kubernetes/operator/crd/FlinkSessionJob.java
@@ -34,7 +34,7 @@ import io.fabric8.kubernetes.model.annotation.Version;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonDeserialize()
 @Group("flink.apache.org")
-@Version("v1alpha1")
+@Version("v1beta1")
 @ShortNames({"sessionjob"})
 public class FlinkSessionJob extends CustomResource<FlinkSessionJobSpec, FlinkSessionJobStatus>
         implements Namespaced {
