4c0d5dc0908184a6b8365e8d3b13f941c401e491
bgeng777 <gengbiao.gb@alibaba-inc.com>
2022-04-19 21:14:12 +0800

    [FLINK-27310] Fix FlinkOperatorITCase

--- flink-kubernetes-operator/src/test/java/org/apache/flink/kubernetes/operator/FlinkOperatorITCase.java
+++ flink-kubernetes-operator/src/test/java/org/apache/flink/kubernetes/operator/FlinkOperatorITCase.java
@@ -114,6 +114,7 @@ public class FlinkOperatorITCase {
         resource.setCpu(1);
         JobManagerSpec jm = new JobManagerSpec();
         jm.setResource(resource);
+        jm.setReplicas(1);
         spec.setJobManager(jm);
         TaskManagerSpec tm = new TaskManagerSpec();
         tm.setResource(resource);
