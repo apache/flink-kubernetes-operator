93110315904496e75e81d4d0ae0c324b4839168b
Lajith <Lajith.Koova@ibm.com>
2025-05-18 23:52:39 +0530

    [FLINK-33634] Add Conditions to Flink CRD's Status field

--- flink-kubernetes-operator-api/src/main/java/org/apache/flink/kubernetes/operator/api/utils/ConditionUtils.java
+++ flink-kubernetes-operator-api/src/main/java/org/apache/flink/kubernetes/operator/api/utils/ConditionUtils.java
@@ -19,7 +19,6 @@ package org.apache.flink.kubernetes.operator.api.utils;
 
 import org.apache.flink.api.common.JobStatus;
 import org.apache.flink.kubernetes.operator.api.status.FlinkDeploymentStatus;
-import org.apache.flink.kubernetes.operator.api.status.JobManagerDeploymentConditionStatus;
 import org.apache.flink.kubernetes.operator.api.status.JobManagerDeploymentStatus;
 
 import io.fabric8.kubernetes.api.model.Condition;
@@ -28,56 +27,13 @@ import io.fabric8.kubernetes.api.model.ConditionBuilder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import static org.apache.flink.api.common.JobStatus.RUNNING;
+import static org.apache.flink.kubernetes.operator.api.status.JobManagerDeploymentStatus.READY;
 
 /** Creates a condition object with the type, status, message and reason. */
 public class ConditionUtils {
     public static final String CONDITION_TYPE_RUNNING = "Running";
-    private static final Map<String, Condition> SESSION_MODE_CONDITION =
-            Map.of(
-                    JobManagerDeploymentStatus.READY.name(),
-                    new ConditionBuilder()
-                            .withType(CONDITION_TYPE_RUNNING)
-                            .withStatus(JobManagerDeploymentConditionStatus.READY.getStatus())
-                            .withReason(JobManagerDeploymentConditionStatus.READY.getReason())
-                            .withMessage(JobManagerDeploymentConditionStatus.READY.getMessage())
-                            .build(),
-                    JobManagerDeploymentStatus.MISSING.name(),
-                    new ConditionBuilder()
-                            .withType(CONDITION_TYPE_RUNNING)
-                            .withStatus(JobManagerDeploymentConditionStatus.MISSING.getStatus())
-                            .withReason(JobManagerDeploymentConditionStatus.MISSING.getReason())
-                            .withMessage(JobManagerDeploymentConditionStatus.MISSING.getMessage())
-                            .build(),
-                    JobManagerDeploymentStatus.DEPLOYING.name(),
-                    new ConditionBuilder()
-                            .withType(CONDITION_TYPE_RUNNING)
-                            .withStatus(JobManagerDeploymentConditionStatus.DEPLOYING.getStatus())
-                            .withReason(JobManagerDeploymentConditionStatus.DEPLOYING.getReason())
-                            .withMessage(JobManagerDeploymentConditionStatus.DEPLOYING.getMessage())
-                            .build(),
-                    JobManagerDeploymentStatus.DEPLOYED_NOT_READY.name(),
-                    new ConditionBuilder()
-                            .withType(CONDITION_TYPE_RUNNING)
-                            .withStatus(
-                                    JobManagerDeploymentConditionStatus.DEPLOYED_NOT_READY
-                                            .getStatus())
-                            .withReason(
-                                    JobManagerDeploymentConditionStatus.DEPLOYED_NOT_READY
-                                            .getReason())
-                            .withMessage(
-                                    JobManagerDeploymentConditionStatus.DEPLOYED_NOT_READY
-                                            .getMessage())
-                            .build(),
-                    JobManagerDeploymentStatus.ERROR.name(),
-                    new ConditionBuilder()
-                            .withType(CONDITION_TYPE_RUNNING)
-                            .withStatus(JobManagerDeploymentConditionStatus.ERROR.getStatus())
-                            .withReason(JobManagerDeploymentConditionStatus.ERROR.getReason())
-                            .withMessage(JobManagerDeploymentConditionStatus.ERROR.getMessage())
-                            .build());
 
     public static Condition getCondition(FlinkDeploymentStatus flinkDeploymentStatus) {
         org.apache.flink.kubernetes.operator.api.status.JobStatus status =
@@ -89,8 +45,8 @@ public class ConditionUtils {
 
             conditionToAdd =
                     jobStatus == null
-                            ? SESSION_MODE_CONDITION.get(
-                                    flinkDeploymentStatus.getJobManagerDeploymentStatus().name())
+                            ? getSessionModeCondition(
+                                    flinkDeploymentStatus.getJobManagerDeploymentStatus())
                             : getApplicationModeCondition(jobStatus);
         }
 
@@ -101,11 +57,12 @@ public class ConditionUtils {
         if (condition == null) {
             return;
         }
-        if (isLastTransactionTimeStampUpdateRequired(conditions, condition)) {
+        Condition existingCondition = conditions.isEmpty() ? null : conditions.get(0);
+        if (isLastTransactionTimeStampUpdateRequired(existingCondition, condition)) {
             condition.setLastTransitionTime(
                     new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(new Date()));
         } else {
-            condition.setLastTransitionTime(conditions.get(0).getLastTransitionTime());
+            condition.setLastTransitionTime(existingCondition.getLastTransitionTime());
         }
     }
 
@@ -118,14 +75,23 @@ public class ConditionUtils {
                 .build();
     }
 
+    private static Condition getSessionModeCondition(JobManagerDeploymentStatus jmStatus) {
+        return new ConditionBuilder()
+                .withType(CONDITION_TYPE_RUNNING)
+                .withStatus(jmStatus == READY ? "True" : "False")
+                .withReason(jmStatus.getReason())
+                .withMessage(jmStatus.getMessage())
+                .build();
+    }
+
     private static String toCamelCase(String reason) {
         reason = reason.toLowerCase();
         return reason.substring(0, 1).toUpperCase() + reason.substring(1);
     }
 
     private static boolean isLastTransactionTimeStampUpdateRequired(
-            List<Condition> conditions, Condition newCondition) {
-        return conditions.isEmpty()
-                || !conditions.get(0).getStatus().equals(newCondition.getStatus());
+            Condition existingCondition, Condition newCondition) {
+        return existingCondition == null
+                || !existingCondition.getStatus().equals(newCondition.getStatus());
     }
 }
