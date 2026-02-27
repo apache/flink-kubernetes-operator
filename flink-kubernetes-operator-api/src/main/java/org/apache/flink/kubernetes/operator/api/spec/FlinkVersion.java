f610d99bbf3d645c2d02ba0a02e4715d9b5b419b
Gyula Fora <g_fora@apple.com>
2026-02-27 09:22:18 +0100

    Update examples to latest flink version

--- flink-kubernetes-operator-api/src/main/java/org/apache/flink/kubernetes/operator/api/spec/FlinkVersion.java
+++ flink-kubernetes-operator-api/src/main/java/org/apache/flink/kubernetes/operator/api/spec/FlinkVersion.java
@@ -43,7 +43,9 @@ public enum FlinkVersion {
     v1_20(1, 20),
     v2_0(2, 0),
     v2_1(2, 1),
-    v2_2(2, 2);
+    v2_2(2, 2),
+    v2_3(2, 3),
+    v2_4(2, 4);
 
     /** The major integer from the Flink semver. For example for Flink 1.18.1 this would be 1. */
     private final int majorVersion;
