2edb20eaa53bc39679bba7bf6fbddf9e2949998b
Purushottam Sinha <psinha@confluent.io>
2026-07-09 15:22:05 +0530

    [hotfix] Apply spotless formatting to fix line length in test comment

--- flink-kubernetes-operator/src/test/java/org/apache/flink/kubernetes/operator/reconciler/deployment/ApplicationReconcilerUpgradeModeTest.java
+++ flink-kubernetes-operator/src/test/java/org/apache/flink/kubernetes/operator/reconciler/deployment/ApplicationReconcilerUpgradeModeTest.java
@@ -428,7 +428,8 @@ public class ApplicationReconcilerUpgradeModeTest extends OperatorTestBase {
 
         // Make sure the upgrade was executed as long as we have the savepoint information
         if (fromMode == UpgradeMode.LAST_STATE && toMode != UpgradeMode.STATELESS) {
-            // We can't make progress as no HA meta available after LAST_STATE, upgrade. It means the
+            // We can't make progress as no HA meta available after LAST_STATE, upgrade. It means
+            // the
             // job started and terminated, but we didn't see...
             assertEquals(
                     JobManagerDeploymentStatus.DEPLOYING,
